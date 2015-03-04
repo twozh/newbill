c08f31793af6466f10958ab0369a26d257d5ec74
twozh <xiaogen-zhang@163.com>
2015-03-04 22:03:18 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//mongodb
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/user");
+
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'user/views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+//use express-session
+var session = require('express-session');
+app.use(session({secret: 'keyboard catt'}));
+
+//use module user
+var user = require('./user');
+app.use('/api/user/', user.userApiRoute);
+app.use('/', user.userRoute);
+app.use(express.static(path.join(__dirname, 'user/public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
